c18529e2baeffbb21c3d4936f4c951935aeeb4c3
hao.h <hao.h@softworld.jp>
2023-11-21 08:53:30 +0000

    (feat) add fastapi router based on directories

--- manage.py
+++ manage.py
@@ -1,12 +1,42 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
 import httpx
 import os
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+from fastapi_directory_routing import DirectoryRouter
+
 
 app = FastAPI(title='ScrapAPI')
 load_dotenv()
 SCRAPY_HOST = 'http://localhost:9080'
+allow_origins = [
+    "http://localhost:80",
+    "localhost:80",
+    "http://0.0.0.0:80",
+    "0.0.0.0:9080",
+    "http://localhost:9080"
+] + os.getenv('', [])
+allow_methods = os.getenv('', ['*'])
+allow_headers = os.getenv('', ['*'])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+app.mount("/", StaticFiles(directory="build", html=True), name="build")
+# Router based on directory
+api_routers = DirectoryRouter(base_directory='routers')
+app.include_router(prefix="/api", router=api_routers)
+
+
+@app.get("/")
+async def index():
+    return RedirectResponse(url="/index.html")
 
 
 @app.get("/api/spider")
---
+++ routers/example/route.py
@@ -0,0 +1,27 @@
+"""
+Handle requests
+"""
+
+
+def get():
+    return {
+        'method': 'GET'
+    }
+
+
+def post():
+    return {
+        'method': 'POST'
+    }
+
+
+def put():
+    return {
+        'method': 'PUT'
+    }
+
+
+def delete():
+    return {
+        'method': 'DELETE'
+    }
